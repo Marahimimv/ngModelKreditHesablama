93ae89d6d3f67d23001486120dd3a49b14a481d8
Faiqmzade3 <faiq.memmedzade3@gmail.com>
2024-02-26 16:24:22 +0400

    KreditHesablamaAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,FormsModule],
+  template: `
+    <h1>Kredit Hesablama</h1>
+    <div>
+      <label>Kredit Tutarı:</label>
+      <input [(ngModel)]="krediTutari">
+    </div>
+    <div>
+      <label>Taksit Tutarı:</label>
+      <select [(ngModel)]="taksitTutari" >
+        <option>1</option>
+        <option>6</option>
+        <option>12</option>
+      </select >
+    </div>
+    <div>
+      <button (click)="hesabla()" >Hesabla</button>
+    </div>
+    <hr>
+    <div>
+      <h4>Aylıq Tutarı: {{ayliqTaksitTutari}} </h4>
+
+    </div>
+
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'KreditHesablamaAngular';
+  krediTutari:number=10000;
+  taksitTutari:number=1;
+  ayliqTaksitTutari:number=0;
+  hesabla(){
+    this.ayliqTaksitTutari=
+      this.krediTutari / this.taksitTutari
+  }
 }
